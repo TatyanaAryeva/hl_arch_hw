53f91002714b176641ad22e7541d6b1e923515f0
T.Grigoryeva <t.grigorieva@arenadata.io>
2025-03-23 22:43:58 +0300

    purify

--- core/src/main/java/org/hlarch/rest/UserRestController.java
+++ core/src/main/java/org/hlarch/rest/UserRestController.java
@@ -17,8 +17,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
 @Slf4j
 @RestController
 @RequestMapping(value = "/api/user", produces = MediaType.APPLICATION_JSON_VALUE)
